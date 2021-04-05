fba8ca795ad61a66396c9183a8aef071689853cc
Masha Krasilnikova <krasilnikova-11@mail.ru>
2021-04-05 12:24:59 +0300

    added button of magnetism

--- src/bsu/rfe/java/group9/Krasilnikova/Field.java
+++ src/bsu/rfe/java/group9/Krasilnikova/Field.java
@@ -1,13 +1,10 @@
 package bsu.rfe.java.group9.Krasilnikova;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 import java.util.ArrayList;
-import javax.swing.JPanel;
-import javax.swing.Timer;
+import javax.swing.*;
+
 @SuppressWarnings("serial")
 public class Field extends JPanel {
     // Флаг приостановленности движения
@@ -15,8 +12,7 @@ public class Field extends JPanel {
     // Динамический список скачущих мячей
     private ArrayList<BouncingBall> balls = new ArrayList<BouncingBall>(10);
     // Класс таймер отвечает за регулярную генерацию событий ActionEvent
-// При создании его экземпляра используется анонимный класс,
-// реализующий интерфейс ActionListener
+// При создании его экземпляра используется анонимный класс, реализующий интерфейс ActionListener
     private Timer repaintTimer = new Timer(10, new ActionListener() {
         public void actionPerformed(ActionEvent ev) {
 // Задача обработчика события ActionEvent - перерисовка окна
@@ -25,9 +21,7 @@ public class Field extends JPanel {
     });
     // Конструктор класса BouncingBall
     public Field() {
-// Установить цвет заднего фона белым
         setBackground(Color.WHITE);
-// Запустить таймер
         repaintTimer.start();
     }
     // Унаследованный от JPanel метод перерисовки компонента
@@ -35,39 +29,30 @@ public class Field extends JPanel {
 // Вызвать версию метода, унаследованную от предка
         super.paintComponent(g);
         Graphics2D canvas = (Graphics2D) g;
-// Последовательно запросить прорисовку от всех мячей из списка
         for (BouncingBall ball: balls) {
             ball.paint(canvas);
         }
     }
-    // Метод добавления нового мяча в список
     public void addBall() {
 //Заключается в добавлении в список нового экземпляра BouncingBall
-// Всю инициализацию положения, скорости, размера, цвета
-// BouncingBall выполняет сам в конструкторе
+// Всю инициализацию положения, скорости, размера, цвета BouncingBall выполняет сам в конструкторе
         balls.add(new BouncingBall(this));
     }
-    // Метод синхронизированный, т.е. только один поток может
-// одновременно быть внутри
+    // Метод синхронизированный, т.е. только один поток может одновременно быть внутри
     public synchronized void pause() {
-// Включить режим паузы
         paused = true;
     }
-    // Метод синхронизированный, т.е. только один поток может
-// одновременно быть внутри
+    // Метод синхронизированный, т.е. только один поток может одновременно быть внутри
     public synchronized void resume() {
-// Выключить режим паузы
         paused = false;
 // Будим все ожидающие продолжения потоки
         notifyAll();
     }
-    // Синхронизированный метод проверки, может ли мяч двигаться
-// (не включен ли режим паузы?)
+    // Синхронизированный метод проверки, может ли мяч двигаться (не включен ли режим паузы?)
     public synchronized void canMove(BouncingBall ball) throws
             InterruptedException {
         if (paused) {
-// Если режим паузы включен, то поток, зашедший
-// внутрь данного метода, засыпает
+// Если режим паузы включен, то поток, зашедший внутрь данного метода, засыпает
             wait();
         }
     }
--- src/bsu/rfe/java/group9/Krasilnikova/MainFrame.java
+++ src/bsu/rfe/java/group9/Krasilnikova/MainFrame.java
@@ -1,46 +1,38 @@
 package bsu.rfe.java.group9.Krasilnikova;
 
-import java.awt.BorderLayout;
-import java.awt.Toolkit;
+import java.awt.*;
 import java.awt.event.ActionEvent;
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+import javax.swing.*;
+
 @SuppressWarnings("serial")
+
 public class MainFrame extends JFrame {
-    // Константы, задающие размер окна приложения, если оно
-// не распахнуто на весь экран
+
     private static final int WIDTH = 700;
     private static final int HEIGHT = 500;
     private JMenuItem pauseMenuItem;
     private JMenuItem resumeMenuItem;
-    // Поле, по которому прыгают мячи
+    private JMenuItem stickMenuItem;
     private Field field = new Field();
     // Конструктор главного окна приложения
     public MainFrame() {
         super("Программирование и синхронизация потоков");
         setSize(WIDTH, HEIGHT);
         Toolkit kit = Toolkit.getDefaultToolkit();
-// Отцентрировать окно приложения на экране
-        setLocation((kit.getScreenSize().width - WIDTH)/2,
-                (kit.getScreenSize().height - HEIGHT)/2);
+        setLocation((kit.getScreenSize().width - WIDTH)/2, (kit.getScreenSize().height - HEIGHT)/2);
 // Установить начальное состояние окна развѐрнутым на весь экран
         setExtendedState(MAXIMIZED_BOTH);
-// Создать меню
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
         JMenu ballMenu = new JMenu("Мячи");
         Action addBallAction = new AbstractAction("Добавить мяч") {
             public void actionPerformed(ActionEvent event) {
                 field.addBall();
-                if (!pauseMenuItem.isEnabled() &&
-                        !resumeMenuItem.isEnabled()) {
+                if (!pauseMenuItem.isEnabled() && !resumeMenuItem.isEnabled() && !stickMenuItem.isEnabled()) {
 // Ни один из пунктов меню не являются
 // доступными - сделать доступным "Паузу"
                     pauseMenuItem.setEnabled(true);
+                    stickMenuItem.setEnabled(true);
                 }
             }
         };
@@ -66,12 +58,19 @@ public class MainFrame extends JFrame {
         };
         resumeMenuItem = controlMenu.add(resumeAction);
         resumeMenuItem.setEnabled(false);
+        Action stickAction = new AbstractAction("Магнетизм") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+        stickMenuItem = controlMenu.add(stickAction);
+        stickMenuItem.setEnabled(false);
+
 // Добавить в центр граничной компоновки поле Field
         getContentPane().add(field, BorderLayout.CENTER);
     }
-    // Главный метод приложения
     public static void main(String[] args) {
-// Создать и сделать видимым главное окно приложения
         MainFrame frame = new MainFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
